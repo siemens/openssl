606565632e2fe24e1ab3a98dfbf696c07d07374b
Rajeev Ranjan <ranjan.rajeev@siemens.com>
2023-11-28 13:28:13 +0100

    obj_{dah.h, mac.num, mac.h}: update NID

--- crypto/objects/obj_dat.h
+++ crypto/objects/obj_dat.h
@@ -1184,7 +1184,7 @@ static const unsigned char so[8476] = {
     0x55,0x1D,0x4B,                                /* [ 8472] OBJ_associated_information */
 };
 
-#define NUM_NID 1320
+#define NUM_NID 1322
 static const ASN1_OBJECT nid_objs[NUM_NID] = {
     {"UNDEF", "undefined", NID_undef},
     {"rsadsi", "RSA Data Security, Inc.", NID_rsadsi, 6, &so[0]},
@@ -2506,9 +2506,11 @@ static const ASN1_OBJECT nid_objs[NUM_NID] = {
     {"altSignatureAlgorithm", "X509v3 Alternative Signature Algorithm", NID_alt_signature_algorithm, 3, &so[8466]},
     {"altSignatureValue", "X509v3 Alternative Signature Value", NID_alt_signature_value, 3, &so[8469]},
     {"associatedInformation", "X509v3 Associated Information", NID_associated_information, 3, &so[8472]},
+    {"eNULL-HMAC-SHA256", "enull-hmac-sha256", NID_enull_hmac_sha256},
+    {"eNULL-HMAC-SHA384", "enull-hmac-sha384", NID_enull_hmac_sha384},
 };
 
-#define NUM_SN 1311
+#define NUM_SN 1313
 static const unsigned int sn_objs[NUM_SN] = {
      364,    /* "AD_DVCS" */
      419,    /* "AES-128-CBC" */
@@ -2983,6 +2985,8 @@ static const unsigned int sn_objs[NUM_SN] = {
     1157,    /* "dstu34311" */
     1159,    /* "dstu4145be" */
     1158,    /* "dstu4145le" */
+    1320,    /* "eNULL-HMAC-SHA256" */
+    1321,    /* "eNULL-HMAC-SHA384" */
      791,    /* "ecdsa-with-Recommended" */
      416,    /* "ecdsa-with-SHA1" */
      793,    /* "ecdsa-with-SHA224" */
@@ -3823,7 +3827,7 @@ static const unsigned int sn_objs[NUM_SN] = {
     1289,    /* "zstd" */
 };
 
-#define NUM_LN 1311
+#define NUM_LN 1313
 static const unsigned int ln_objs[NUM_LN] = {
      363,    /* "AD Time Stamping" */
      405,    /* "ANSI X9.62" */
@@ -4366,6 +4370,8 @@ static const unsigned int ln_objs[NUM_LN] = {
       48,    /* "emailAddress" */
      632,    /* "encrypted track 2" */
      885,    /* "enhancedSearchGuide" */
+    1320,    /* "enull-hmac-sha256" */
+    1321,    /* "enull-hmac-sha384" */
     1267,    /* "ess-attributes" */
     1265,    /* "etsi" */
       56,    /* "extendedCertificateAttributes" */
